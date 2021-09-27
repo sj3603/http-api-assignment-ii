284806bce5e6f8a4dd8a829e0971c9061f3a03c1
sj3603 <56056374+sj3603@users.noreply.github.com>
2021-09-26 21:37:05 -0400

    finshed assignment

---
+++ src/jsonResponses.js
@@ -0,0 +1,82 @@
+// Note this object is purely in memory
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
+// public exports
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  addUser,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,106 @@
+const http = require('http'); // pull in http module
+// url module for parsing url string
+const url = require('url');
+// querystring module for parsing querystrings from url
+const query = require('querystring');
+// pull in our custom files
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  switch (parsedUrl.pathname) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      htmlHandler.getCSS(request, response);
+      break;
+    case '/getUsers':
+      jsonHandler.getUsers(request, response);
+      break;
+    case '/notReal':
+      jsonHandler.notFound(request, response);
+      break;
+    default:
+      jsonHandler.notFound(request, response);
+      break;
+  }
+};
+
+// handle HEAD requests
+const handleHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsersMeta(request, response);
+  } else {
+    jsonHandler.notFoundMeta(request, response);
+  }
+};
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // if post is to /addUser (our only POST url)
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+
+    // uploads come in as a byte stream that we need
+    // to reassemble once it's all arrived
+    const body = [];
+
+    // if the upload stream errors out, just throw a
+    // a bad request and send it back
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    // on 'data' is for each byte of data that comes in
+    // from the upload. We will add it to our byte array.
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // on end of upload stream.
+    request.on('end', () => {
+      // combine our byte array (using Buffer.concat)
+      // and convert it to a string value (in this instance)
+      const bodyString = Buffer.concat(body).toString();
+      // since we are getting x-www-form-urlencoded data
+      // the format will be the same as querystrings
+      // Parse the string into an object by field name
+      const bodyParams = query.parse(bodyString);
+
+      // pass to our addUser function
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  }
+};
+
+const onRequest = (request, response) => {
+  // parse url into individual parts
+  // returns an object of url parts by name
+  const parsedUrl = url.parse(request.url);
+
+  // check if method was POST, GET, or HEAD
+  switch (request.method) {
+    case 'GET':
+      handleGet(request, response, parsedUrl);
+      break;
+    case 'HEAD':
+      handleHead(request, response, parsedUrl);
+      break;
+    case 'POST':
+      handlePost(request, response, parsedUrl);
+      break;
+    default:
+      jsonHandler.notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(PORT);
+
+console.log(`Listening on 127.0.0.1: ${PORT}`);
